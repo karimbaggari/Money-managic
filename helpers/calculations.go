575ed89fdf60ecfd619c8d826073819a479a100f
Karim <kariim.baggarii@gmail.com>
2024-07-01 14:59:31 +0100

    update the calculation function to calculate also the savings

--- helpers/calculations.go
+++ helpers/calculations.go
@@ -19,11 +19,11 @@ func CalculateLivingBudget(incomesId string, expensesId string) (model.UserLivin
 
 	collection := database.GetCollection(dbName, dbCollection1)
 
-	 objectID, err := primitive.ObjectIDFromHex(incomesId)
-	 if err != nil {
-		 fmt.Println("Invalid ObjectID:", err)
-	 }
- 
+	objectID, err := primitive.ObjectIDFromHex(incomesId)
+	if err != nil {
+		fmt.Println("Invalid ObjectID:", err)
+	}
+
 	// Query document by ObjectID
 	var incomesModel model.UserIncomes
 	err = collection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&incomesModel)
@@ -31,38 +31,37 @@ func CalculateLivingBudget(incomesId string, expensesId string) (model.UserLivin
 		fmt.Println("issue into finding the data of the user check the user Id if it's valid ")
 	}
 
-
 	collection = database.GetCollection(dbName, dbCollection2)
 
 	objectID, err = primitive.ObjectIDFromHex(expensesId)
-	 if err != nil {
-		 fmt.Println("Invalid ObjectID:", err)
-	 }
+	if err != nil {
+		fmt.Println("Invalid ObjectID:", err)
+	}
 
-	 	// Query document by ObjectID
+	// Query document by ObjectID
 	var expensesModel model.UserExpenses
-	err = collection.FindOne(context.Background(), bson.M{"_id":objectID}).Decode(&expensesModel)
+	err = collection.FindOne(context.Background(), bson.M{"_id": objectID}).Decode(&expensesModel)
 	if err != nil {
 		fmt.Println("issue into finding the data of the user check the user Id if it's valid ")
 	}
 
-	// fmt.Println("incomes ",incomesModel, "expenses", expensesModel)
 
-	//savingPercentage := 0.2
-	result := (incomesModel.Incomes + incomesModel.PassiveIncome ) - ( expensesModel.ConstantExpenses + expensesModel.ExtraExpenses)
+	savingPercentage := 0.2
+	result := (incomesModel.Incomes + incomesModel.PassiveIncome) - (expensesModel.ConstantExpenses + expensesModel.ExtraExpenses)
 	fmt.Println("the rest is ", result)
-	// var budget model.UserLivingBudget
-	// if result >= 3000 {
-	// 	budget.MinimalLivingBudget = result
-	// 	budget.MaximalLivingBudget = result
-	// 	return budget, nil
-	// } else {
-	// 	for result >= 3000 {
-	// 		savingPercentage = savingPercentage - 0.01
-	// 		result = incomesValue.MinimalLivingBudget - expensesValue - (incomesValue.MinimalLivingBudget * savingPercentage)
-	// 	}
-	// 	budget.MinimalLivingBudget = result
-	// 	return budget, nil
-	// }
-	return model.UserLivingBudget{}, nil
+	var budget model.UserLivingBudget
+	if result >= 3000 {
+		amountToSave := result * savingPercentage
+		budget.MaximalLivingBudget = result - amountToSave
+		fmt.Println("the value of savings is ", budget.MaximalLivingBudget)
+		return budget, nil
+	} else {
+		for result >= 3000 {
+			savingPercentage = savingPercentage - 0.01
+			budget.MinimalLivingBudget = budget.MinimalLivingBudget - (expensesModel.ConstantExpenses + expensesModel.ExtraExpenses) - (budget.MinimalLivingBudget * savingPercentage)
+		}
+		fmt.Println("the value is ", budget.MinimalLivingBudget)
+		return budget, nil
+	}
+
 }
